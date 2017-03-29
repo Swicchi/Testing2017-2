e02785e93904b44948852cce5accf1cc769184ce
remferna <remferna@alumnos.ubiobio.cl>
2017-03-29 12:25:38 -0300

    Se agrega test con numeros negativos

--- src/main/java/cl/ubb/testing/MyMath.java
+++ src/main/java/cl/ubb/testing/MyMath.java
@@ -1,5 +1,8 @@
 package cl.ubb.testing;
 
 public class MyMath {
-
+	
+	public int Add(int a, int b){
+		return a+b;
+	}
 }
--- src/test/java/cl/ubb/testing/TestMyMath.java
+++ src/test/java/cl/ubb/testing/TestMyMath.java
@@ -1,5 +1,23 @@
+
 package cl.ubb.testing;
 
-public class TestMyMath {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class TestMyMath {
+	@Test
+	public void AddTwoAndTwoIsFour(){
+		MyMath myMath = new MyMath();
+		int resultado;
+		resultado = myMath.Add(2, 2);
+		assertEquals(resultado,4);
+	}
+	@Test
+	public void AddMinusOneAndFourIsThree(){
+		MyMath myMath = new MyMath();
+		int resultado;
+		resultado = myMath.Add(-1, 4);
+		assertEquals(resultado,3);
+	}
 }
